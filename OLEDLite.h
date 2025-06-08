86eddc037110a17e88f9c424316bfbca6454de35
slavatlsn <93151297+slavatlsn@users.noreply.github.com>
2025-06-09 01:34:23 +0300

    Update OLEDLite.h
    polymorphed

--- OLEDLite.h
+++ OLEDLite.h
@@ -7,14 +7,14 @@
 class Display
 {
   private:
-    uint8_t adr;
+    uint8_t adr, w, h;
     static const uint8_t I2C_BUFFER_LIMIT = 30;
 
     void send_command(uint8_t);
     void send_command(uint8_t, uint8_t);
 
   public:
-    Display(uint8_t);
+    Display(uint8_t, uint8_t, uint8_t);
     void init();
     bool set_pos(uint8_t, uint8_t);
     void send_data(uint8_t);
